3fb2692fe7acdd42dcec4d265e8848e48a85c611
unknown <mdfuadulislamhridoy52@gmail.com>
2020-12-19 00:13:35 +0600

    Problem solved

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -5,14 +5,7 @@ const News = (props) => {
 
     return (
         <div>
-            <Card>
-                <Card.Header as="h5">{props.article.title}</Card.Header>
-                    <Card.Body>
-                        <Card.Title></Card.Title>
-                            <Card.Text></Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-            </Card>
+            
         </div>
     );
 };
--- src/components/TopHeadline/TopHeadline.js
+++ src/components/TopHeadline/TopHeadline.js
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import News from '../News/News';
+import { Card, Button } from 'react-bootstrap';
 
 const TopHeadline = () => {
-    const [articles, setArticles] = useState([]);
+    const [check, setCheck] = useState([]);
 
     useEffect(() => {
         fetch('http://newsapi.org/v2/everything?q=bitcoin&from=2020-11-18&sortBy=publishedAt&apiKey=d3900ba57fb541ddb735538e12d7f238')
         .then(res => res.json())
-        .then(data => setArticles(data.articles))
+        .then(data => setCheck(data.articles))
     }, [])
     return (
         <div>
-            <h1>News: {articles.length}</h1>
             {
-                articles.map(article => <News article={article} ></News>)
+                check.map(article => {
+                    return(
+                        <Card>
+                            <Card.Header as="h5">{article.author}</Card.Header>
+                                <Card.Body>
+                                    <Card.Title>{article.title}</Card.Title>
+                                        <Card.Text>{article.description}</Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                        </Card>
+                        
+                    )
+                })
             }
-            {/* <li>{article.title}</li> */}
         </div>
     );
 };
